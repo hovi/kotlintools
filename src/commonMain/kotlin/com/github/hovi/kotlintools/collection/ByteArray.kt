a5cc2a4826b0a9b97d3bf83b5042de211e322904
Karel Hovorka <info@karel-hovorka.eu>
2019-11-23 11:04:16 +0100

    bytes for UTF8 replacement character

--- src/commonMain/kotlin/com/github/hovi/kotlintools/collection/ByteArray.kt
+++ src/commonMain/kotlin/com/github/hovi/kotlintools/collection/ByteArray.kt
@@ -3,6 +3,7 @@ package com.github.hovi.kotlintools.collection
 val UTF8_INVALID_CHARACTER_BYTES = byteArrayOf(-17, -65, -67)
 
 val UTF8_BOM = byteArrayOf(0xEF.toByte(), 0xBB.toByte(), 0xBF.toByte())
+val UTF8_REPLACEMENT = byteArrayOf(0xef.toByte(), 0xbf.toByte(), 0xbd.toByte())
 
 fun ByteArray.countSubArray(subarray: ByteArray): Int {
     if (subarray.isEmpty()) {

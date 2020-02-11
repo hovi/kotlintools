6fe4fb71a60d8bdebaa8ab333e8767c204b8c6f3
Karel Hovorka <info@karel-hovorka.eu>
2020-02-11 16:25:51 +0100

    time measurements

--- src/commonMain/kotlin/com/github/hovi/kotlintools/datetime/DatetimeUtil.kt
+++ src/commonMain/kotlin/com/github/hovi/kotlintools/datetime/DatetimeUtil.kt
@@ -1,3 +1,20 @@
 package com.github.hovi.kotlintools.datetime
 
 expect val timestamp: Long
+
+class TimeMeasure(var startTime: Long = timestamp) {
+    fun restart() {
+        startTime = timestamp
+    }
+
+    fun end(): Long {
+        return timestamp - startTime
+    }
+}
+
+
+public inline fun measureTimeMillis(block: () -> Unit): Long {
+    val start = timestamp
+    block()
+    return timestamp - start
+}

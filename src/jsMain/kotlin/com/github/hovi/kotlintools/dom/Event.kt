2e867c6b661c5caeb71785da722dd06ad1734332
Karel Hovorka <info@karel-hovorka.eu>
2020-03-05 15:33:00 +0100

    throttleEvent

--- src/jsMain/kotlin/com/github/hovi/kotlintools/dom/Event.kt
+++ src/jsMain/kotlin/com/github/hovi/kotlintools/dom/Event.kt
@@ -3,17 +3,33 @@ package com.github.hovi.kotlintools.dom
 import org.w3c.dom.events.Event
 import kotlin.browser.window
 
-fun <E : Event> debounceEvent(callback: () -> Unit, milliseconds: Int, immediate: Boolean = false): (E) -> Unit {
+fun <E : Event> debounceEvent(callback: (Event) -> Unit, milliseconds: Int, immediate: Boolean = false): (E) -> Unit {
     var timeout: Int? = null
 
     return fun(event: E) {
         val firstRun = timeout == null
         timeout?.let { window.clearTimeout(it) }
         timeout = window.setTimeout({
-            callback()
+            callback(event)
         }, milliseconds)
         if (firstRun && immediate) {
-            callback()
+            callback(event)
         }
+    }
+}
+
+fun <E : Event> throttleEvent(callback: (Event) -> Unit, milliseconds: Int = 250): (E) -> Unit {
+    var inThrottle = false
+
+    return fun(event: E) {
+        if (inThrottle) {
+            return
+        }
+        callback(event)
+        inThrottle = true
+        window.setTimeout({
+            inThrottle = false
+        }, milliseconds)
+
     }
 }
